499b1ddea3f2889f9762427271cd6ca028bdf79d
Valery Gruzdev <vgrusdev@gmail.com>
2025-03-17 19:59:24 +0300

    Fix an errors

--- lib/sapcontrol/client.go
+++ lib/sapcontrol/client.go
@@ -1,13 +1,13 @@
 package sapcontrol
 
 import (
-	"context"
-	"net"
-	"net/http"
+	//"context"
+	//"net"
+	//"net/http"
 
 	"github.com/hooklift/gowsdl/soap"
-	"github.com/spf13/viper"
-	//"github.com/vgrusdev/sap_system_exporter/internal/config"
+	//"github.com/spf13/viper"
+	"github.com/vgrusdev/sap_system_exporter/internal/config"
 )
 
 type MyClient struct {
