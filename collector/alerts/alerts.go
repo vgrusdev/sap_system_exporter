4b070894f031bf17ff75eb74f9e555b7c5692308
Valery Gruzdev <vgrusdev@gmail.com>
2025-11-15 13:24:44 +0300

    added alert_samples_max_age parameter

--- collector/alerts/alerts.go
+++ collector/alerts/alerts.go
@@ -1,19 +1,19 @@
 package alerts
 
 import (
-	"strconv"
 	"fmt"
-	"time"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/prometheus/client_golang/prometheus"
 
+	"github.com/vgrusdev/promtail-client/promtail"
 	"github.com/vgrusdev/sap_system_exporter/collector"
 	"github.com/vgrusdev/sap_system_exporter/lib/sapcontrol"
-	"github.com/vgrusdev/promtail-client/promtail"
 )
 
 func NewCollector(webService sapcontrol.WebService) (*alertsCollector, error) {
@@ -47,10 +47,10 @@ func (c *alertsCollector) Collect(ch chan<- prometheus.Metric) {
 	}
 }
 
-type current_alert  struct {
+type current_alert struct {
 	Object      string
 	Attribute   string
-	Value		sapcontrol.STATECOLOR
+	Value       sapcontrol.STATECOLOR
 	Description string
 	ATime       string
 }
@@ -59,7 +59,7 @@ func labelSetFromArrays(keys []string, values []string) (map[string]string, erro
 	m := make(map[string]string)
 	if len(keys) != len(values) {
 		return m, errors.New("Arrays should be the same length")
-	}	
+	}
 	for i, key := range keys {
 		m[key] = values[i]
 	}
@@ -75,7 +75,7 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 		return errors.Wrap(err, "SAPControl web service Alerts error")
 	}
 
-	log.Debugf("Alerts: Instances in the list: %d", len(instanceList.Instances) )
+	log.Debugf("Alerts: Instances in the list: %d", len(instanceList.Instances))
 
 	client := c.webService.GetMyClient()
 	useHTTPS := client.Config.UseHTTPS()
@@ -83,8 +83,15 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 	if err != nil {
 		return errors.Wrap(err, "SAPControl config Copy error")
 	}
-	send_to_prom := client.Config.Viper.GetBool("send_alerts_to_prom")	// send_alerts_to_prom = bool in config file
-	if send_to_prom == true {
+	send_to_prom := client.Config.Viper.GetBool("send_alerts_to_prom")            // send_alerts_to_prom = bool in config file
+	samples_max_age_str := client.Config.Viper.GetString("alert_samples_max_age") // Oldest accespted timestamp for alert, golang Duration string, to avoid errors/wrnings in Loki
+	var samples_max_age time.Duration
+	if samples_max_age_str == "" {
+		samples_max_age = 0 * time.Second
+	} else {
+		samples_max_age, _ = time.ParseDuration(samples_max_age_str)
+	}
+	if send_to_prom {
 		log.Debugln("Will send alerts to prom")
 	} else {
 		log.Debugln("Will not send Alerts to Prom")
@@ -98,7 +105,7 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 	if loki_client != nil {
 		promDesc := c.GetDescriptor("Alert")
 		promDescString := promDesc.String()
-		
+
 		log.Debugf("promDescriptorString = %s", promDescString)
 
 		_, after, _ := strings.Cut(promDescString, "variableLabels: {")
@@ -111,7 +118,7 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 	for _, instance := range instanceList.Instances {
 
 		url := ""
-		if useHTTPS == true {
+		if useHTTPS {
 			url = fmt.Sprintf("https://%s:%d", instance.Hostname, instance.HttpsPort)
 		} else {
 			url = fmt.Sprintf("http://%s:%d", instance.Hostname, instance.HttpPort)
@@ -129,7 +136,7 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 			log.Warnf("SAPControl web service error: %s", err)
 			continue
 		}
-		commonLabels := []string {
+		commonLabels := []string{
 			currentSapInstance.Name,
 			strconv.Itoa(int(currentSapInstance.Number)),
 			currentSapInstance.SID,
@@ -146,7 +153,7 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 
 		for _, alert := range alertList.Alerts {
 
-			alert_item := current_alert {
+			alert_item := current_alert{
 				Object:      alert.Object,
 				Attribute:   alert.Attribute,
 				Value:       alert.Value,
@@ -155,13 +162,13 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 			}
 			alert_item_list = append(alert_item_list, alert_item)
 		}
-		if send_to_prom == true {
+		if send_to_prom {
 			// Remove duplicates for Prometheus
-			log.Debugf("Alerts in the list BEFORE remove duplicates: %d", len(alert_item_list) )
+			log.Debugf("Alerts in the list BEFORE remove duplicates: %d", len(alert_item_list))
 			alert_item_list = sapcontrol.RemoveDuplicate(alert_item_list)
-			log.Debugf("Alerts in the list AFTER remove duplicates: %d", len(alert_item_list) )
+			log.Debugf("Alerts in the list AFTER remove duplicates: %d", len(alert_item_list))
 		} else {
-			log.Debugf("Alerts in the list: %d", len(alert_item_list) )
+			log.Debugf("Alerts in the list: %d", len(alert_item_list))
 		}
 		for _, alert_item := range alert_item_list {
 
@@ -170,18 +177,17 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 				log.Warnf("SAPControl web service error, unable to process SAPControl Alert Value data %v: %s", alert_item.Value, err)
 				continue
 			}
-			labels := append([]string{	alert_item.Object, 
-										alert_item.Attribute, 
-										alert_item.Description, 
-										alert_item.ATime, 
-										string(alert_item.Value) },
-								commonLabels...)
-			
-			
+			labels := append([]string{alert_item.Object,
+				alert_item.Attribute,
+				alert_item.Description,
+				alert_item.ATime,
+				string(alert_item.Value)},
+				commonLabels...)
+
 			// Response to Prometheus request (may be to setup IF...  TODO )
 			// need to check what will be if I will not send anything to prom.
 			// or should I send something small....
-			if send_to_prom == true {
+			if send_to_prom {
 				ch <- c.MakeGaugeMetric("Alert", state, labels...)
 			}
 
@@ -193,25 +199,28 @@ func (c *alertsCollector) recordAlerts(ch chan<- prometheus.Metric) error {
 					continue
 				}
 				message := string(labelSet["Message"])
-				aTime   := string(labelSet["ATime"])
+				aTime := string(labelSet["ATime"])
 
 				t, err := time.ParseInLocation(timeFormat, aTime, timeLocation)
 				if err != nil {
-					fmt.Printf("Warning: %s\n", err)
+					log.Warnf("Alert ATime parsing: %s", err)
 					t = time.Now()
+				}
+				if time.Since(t) > samples_max_age {
+					log.Warnf("Alert entry too far behind, ts=%v", t)
 				}
 				delete(labelSet, "Message")
 				delete(labelSet, "ATime")
 				labelSet["level"], _ = sapcontrol.StateColorToLevel(alert_item.Value)
-	
-				sInputEntry := promtail.SingleEntry {
-					Labels:  labelSet,
-					Ts:    	t,
-					Line:  	message,
+
+				sInputEntry := promtail.SingleEntry{
+					Labels: labelSet,
+					Ts:     t,
+					Line:   message,
 				}
 				loki_client.Single() <- &sInputEntry
-			} 	// if loki_client != nil
-		}		// for _, alert_item := range alert_item_list
-	}			// for _, instance := range instanceList.Instances
+			} // if loki_client != nil
+		} // for _, alert_item := range alert_item_list
+	} // for _, instance := range instanceList.Instances
 	return nil
 }
--- go.mod
+++ go.mod
@@ -16,6 +16,11 @@ require (
 )
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
+)
+
+require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -37,6 +42,7 @@ require (
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/vgrusdev/promtail-client v0.0.3
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
